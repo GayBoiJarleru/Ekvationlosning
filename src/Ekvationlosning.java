7864ed8d1f10342e1f111b61d9ff6f17c5b3f50d
GayBoiJarleru <jarl.bergstrom@elev.ga.ntig.se>
2019-10-24 11:12:13 +0200

    Papew

--- src/Ekvationlosning.java
+++ src/Ekvationlosning.java
@@ -1,2 +1,16 @@
+import javax.swing.*;
+
+/*
+Skapad av Jarl Bergström
+2019-10-24
+ */
+
 public class Ekvationlosning {
+    public class void main(String[] args) {
+        JOptionPane.showInputDialog(null "Skriv ditt K värde");
+        JOptionPane.showInputDialog(null "Skriv ditt M värde");
+        JOptionPane.showInputDialog(null "Skriv ditt Y värde");
+
+
+    }
 }
